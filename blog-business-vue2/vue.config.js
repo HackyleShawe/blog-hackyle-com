1c2b52be8c6e5cbb8b437005e1b75c40054c3aa5
Hackyle <kyleshawe@outlook.com>
2022-11-02 16:24:31 +0800

    change: redefine service's port of business of vue

--- blog-business-vue2/vue.config.js
+++ blog-business-vue2/vue.config.js
@@ -1,31 +1,35 @@
 // const { defineConfig } = require('@vue/cli-service')
 
+//从配置文件中读取项目使用的端口
+let port = process.env.PORT
+let backendAPI = process.env.BACKEND_API
+
 module.exports = {
   // transpileDependencies: true,
 
-  lintOnSave: false,
+  lintOnSave: process.env.NODE_ENV === 'development', //如果是开发环境，就是true
 
   devServer: {
-    host: 'localhost',//本机ip
-    port: 8010,//本项目使用的端口
+    host: 'localhost', //本机ip
+    port: port, //本项目使用的端口
     open: true,
-    // proxy: {
-    //     '/': {  //代理别名
-    //         target: 'http://localhost:9010',   //代理目标值
-    //         changeOrigin: true,
-    //         secure: true,
-    //         地址栏显示：http://localhost:7878/#/login
-    //         尽管请求地址显示的是：http://localhost:7878/api/login
-    //         但实际访问的却是：tartget/login
-    //         也就是说，域名被代理了，但路由地址是不变的
-    //         pathRewrite:{  //替换路径中的/api
-    //             '^/api':'/'
-    //         }
-    //         pathRequiresRewrite: {
-    //           '^/api': ''
-    //         }
-    //     }
-    // },
+     proxy: {
+         '/': {  //代理别名
+             target: backendAPI,   //代理目标值
+             changeOrigin: true,
+             secure: true,
+             //地址栏显示：http://localhost:7878/#/login
+             //尽管请求地址显示的是：http://localhost:7878/api/login
+             //但实际访问的却是：tartget/login
+             //也就是说，域名被代理了，但路由地址是不变的
+             pathRewrite:{  //替换路径中的/api
+                 '^/':'/'
+             }
+             //pathRequiresRewrite: {
+             //  '^/api': ''
+             //}
+         }
+     },
     overlay: {
       warning: false,
       errors: true
